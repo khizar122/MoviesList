7fc43f678eb90ae949c687246b0294a38e4a9005
khizar122 <raokhizar49@gmail.com>
2022-07-06 12:57:53 +0500

    remove warnings

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -7,19 +7,19 @@ import logo from './images/logo.png'
 const NavBar = () => {
   return (
     <div>
-        <nav className={styles.navbar}>
-        <a href="#" className={styles.logo}>
+        <nav >
+       
         <img src={logo} alt="Logo" />
-        </a>
+        
         
 
         <ul className={styles.links}>
 
 
-          <li className={styles.nav}><a href="#"> SignIn </a></li>
-          <li className={styles.nav}><a href="#"> Premium</a></li>
-          <li className={styles.nav}><a href="#"> About Us</a></li>
-          <li className={styles.nav}><a href="#"> Contact Us</a></li>
+          <li className={styles.nav}>SignIn </li>
+          <li className={styles.nav}> Premium</li>
+          <li className={styles.nav}>About Us</li>
+          <li className={styles.nav}>Contact Us</li>
         </ul>
       </nav>
 
